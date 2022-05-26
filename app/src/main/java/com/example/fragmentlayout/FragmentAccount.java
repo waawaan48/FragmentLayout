b4937489acf381d2ac4ce9500bfeb0a6122804f8
Shafwan Hidayat A. F55119114 <waawaan48@gmail.com>
2022-05-26 17:12:17 +0800

    Initial commit

---
+++ app/src/main/java/com/example/fragmentlayout/FragmentAccount.java
@@ -0,0 +1,23 @@
+package com.example.fragmentlayout;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentAccount extends Fragment {
+    View view;
+    public FragmentAccount() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.account_fragment, container,false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/fragmentlayout/FragmentHome.java
@@ -0,0 +1,23 @@
+package com.example.fragmentlayout;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentHome extends Fragment {
+    View view;
+    public FragmentHome() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.home_fragment, container,false);
+        return view;
+    }
+}
